c8803d2d20f2b8befb7786a6318ad15cdc48f111
Vladimír Vondruš <mosra@centrum.cz>
2025-01-07 19:19:10 +0100

    package: make generate-loc.sh work independently of CWD.

--- package/generate-loc.sh
+++ package/generate-loc.sh
@@ -1,3 +1,9 @@
-for i in $(ls *.h *.hpp); do
+#!/bin/bash
+
+set -e
+
+dir=$(dirname "$0")
+
+for i in $(ls $dir/../*.{h,hpp}); do
     echo "**[$i]($i)** |" $(cat $i | wc -l) '|' $(echo "#include \"$i\"" | g++ -std=c++11 -P -E -x c++ - | wc -l)
 done
